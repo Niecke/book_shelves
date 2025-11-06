b6f1b72fe893225ae81f378f151d5efd45e58464
Daniel Niecke <daniel.niecke@worldline.com>
2025-11-06 13:31:24 +0100

    add dummy for database selects

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from models import db
+from flask import Flask, jsonify
 from flask_migrate import upgrade, Migrate
+from models import db, InviteCode
 import env
 
 
@@ -19,9 +19,23 @@ if env.FLASK_AUTO_UPGRADE == "true":
         upgrade()
 
 
-@app.route("/")
-def hello_world():
+@app.route("/", methods=["GET"])
+def index():
     return "Hello, World! Version 4"
+
+
+@app.route("/codes", methods=["GET"])
+def get_invite_codes():
+    invite_codes = InviteCode.query.all()  # Fetch all rows
+    result = [
+        {
+            "id": invite_code.id,
+            "code": invite_code.code,
+            "created_at": invite_code.created_at,
+        }
+        for invite_code in invite_codes
+    ]
+    return jsonify(result)
 
 
 if __name__ == "__main__":
