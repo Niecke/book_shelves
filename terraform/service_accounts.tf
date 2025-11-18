4c01ed0eff8a4811ec255fad59bedf2ada0236ed
Daniel Niecke <daniel.niecke@worldline.com>
2025-11-18 12:30:59 +0100

    adding grants for oauth secrets

--- terraform/service_accounts.tf
+++ terraform/service_accounts.tf
@@ -10,6 +10,19 @@ resource "google_secret_manager_secret_iam_member" "cloud_run_access" {
   member    = "serviceAccount:${google_service_account.cloud_run_sa.email}"
 }
 
+# These secrets are managed outside of this terraform code, but we need to grant access to them
+resource "google_secret_manager_secret_iam_member" "cloud_run_access" {
+  secret_id = "projects/1018120441073/secrets/GOOGLE_CLIENT_ID"
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.cloud_run_sa.email}"
+}
+
+resource "google_secret_manager_secret_iam_member" "cloud_run_access" {
+  secret_id = "projects/1018120441073/secrets/GOOGLE_CLIENT_SECRET"
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.cloud_run_sa.email}"
+}
+
 resource "google_project_iam_member" "cloud_run_sql_access" {
   project = var.project_id
   role    = "roles/cloudsql.client"
